289d9b02c1ba220238d87437b9689f6badc48c22
Aishwarya Manickam Rameshbabu <aishukavibabu@gmail.com>
2021-08-14 12:07:14 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,84 @@
+
+
+document.querySelector(".button-container").addEventListener("click", () => {
+    let searchText = document.getElementById("filter-jobs").value;
+    getJobs().then(jobs => {
+        let filteredJobs = filterJobs(jobs, searchText);
+        showJobs(filteredJobs);
+    })
+    
+})
+
+function getJobs(){
+    return fetch("data.json")
+    .then(response => response.json())
+    .then(data => {
+        return data;
+    })
+}
+
+function filterJobs(jobs, searchText){
+
+    if(searchText){
+        let filteredItems = jobs.filter(job => {
+            if(job.roleName.toLowerCase().includes(searchText) ||
+            job.company.toLowerCase().includes(searchText) ||
+            job.type.toLowerCase().includes(searchText) ||
+            job.location.toLowerCase().includes(searchText) ||
+            job.requirements.content.toLowerCase().includes(searchText)){
+                return true;
+            }
+            else return false;
+        }) 
+        return filteredItems;}
+
+        else return jobs;
+}
+
+function showJobs(jobs){
+
+    let jobsContainer = document.querySelector(".jobs-container");
+
+    let jobsHTML = "";
+    jobs.forEach(job => {
+
+        jobsHTML += `
+        
+        <div class="job-tile">
+                <div class="top">
+                    <img src= "${job.logo}">
+
+                    <span class="material-icons more_horiz">more_horiz</span>
+                </div>
+
+                <div class="rolename">
+                    <span>${job.roleName}</span>
+                </div>
+
+                <div class="description">
+                    <span>${job.requirements.content}</span>
+                </div>
+
+                <div class="buttons">
+                    <div class="button apply-now">
+                        Apply now
+                    </div>
+
+                    <div class="button">
+                        Message
+                    </div>
+                </div>
+
+            </div>
+        `
+        
+    });
+
+    jobsContainer.innerHTML = jobsHTML;
+
+}
+
+//when application is initially opened
+getJobs().then(data => {
+    showJobs(data);
+});
